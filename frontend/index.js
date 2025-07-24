481f74748ae1314ea573285e2bf85138ee4b92a8
atharvyadav96k <atharvyadav96k@gmail.com>
2025-07-24 14:10:41 +0530

    joined frontend and backend

---
+++ frontend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+    res.render('index', { title: 'Home Page' });
+});
+
+app.get('/getdata', (req, res)=>{
+    fetch("http://172.17.0.3:3000/")
+    .then(response => response.json())
+    .then(data => {
+        res.json(data);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error); 
+        res.status(500).json({ error: 'Failed to fetch data' });
+    });
+});
+
+app.listen(port, () => { 
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Basic Backend 
+Backend is running on port 300
+
+Single will send message /
+```
+{
+  "message": "Hello, World!"
+}
+```
